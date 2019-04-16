f510a8d5b66554fd75b15009ab8ec5cb20157fa5
Zeroto521 <Zeroto521@gmail.com>
2019-04-16 23:21:20 +0800

    :white_check_mark: update test.py

--- test.py
+++ test.py
@@ -1,29 +1,34 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
+from sklearn.datasets import load_iris
 
 from compress import compress
 
+# load iris data set
+iris = load_iris()
+data = iris.data
+labels = iris.target
 
-df = pd.read_csv('data/iris.csv')
-labels = df.values[:, -1]
-data = df.values[:, :-1]
+# next let compress the data
 
+# k = 0
 data_new = compress(data, labels, k=0)  # k defalut is 0
 length, length_new = len(data), len(data_new)
 print("old : new = {} : {}".format(length, length_new))
 
+# k = 1
 data_new = compress(data, labels, k=1)
 length, length_new = len(data), len(data_new)
 print("old : new = {} : {}".format(length, length_new))
 
+# k = 1
 data_new = compress(data, labels, k=2)
 length, length_new = len(data), len(data_new)
 print("old : new = {} : {}".format(length, length_new))
 
 
 """result
-old : new = 150 : 126
-old : new = 150 : 65
-old : new = 150 : 20
+old : new = 150 : 129  # k = 0
+old : new = 150 : 66  # k = 1
+old : new = 150 : 20  # k = 2
 """
