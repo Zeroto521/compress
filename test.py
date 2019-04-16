451a56981c58e5e530ef0f642b99bbaf2cabfca3
Zeroto521 <Zeroto521@gmail.com>
2019-04-16 23:16:24 +0800

    Update test.py

--- test.py
+++ test.py
@@ -9,8 +9,21 @@ df = pd.read_csv('data/iris.csv')
 labels = df.values[:, -1]
 data = df.values[:, :-1]
 
-data_new = compress(data, labels)
+data_new = compress(data, labels, k=0)  # k defalut is 0
 length, length_new = len(data), len(data_new)
-
-print("Data size")
 print("old : new = {} : {}".format(length, length_new))
+
+data_new = compress(data, labels, k=1)
+length, length_new = len(data), len(data_new)
+print("old : new = {} : {}".format(length, length_new))
+
+data_new = compress(data, labels, k=2)
+length, length_new = len(data), len(data_new)
+print("old : new = {} : {}".format(length, length_new))
+
+
+"""result
+old : new = 150 : 126
+old : new = 150 : 65
+old : new = 150 : 20
+"""
